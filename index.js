28f16f4a781e22444227e89ef310449979f4f335
Sarath V S <sarathvss65@gmail.com>
2020-01-20 10:57:08 +0530

    Inject information from js layer using vue.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    name: { first: "Sarath", last: "Sasindrakumar" },
+    description:
+      "I am a Full Stack Developer, a Traveller and a Photographer based in Bangalore, India.",
+    socials: [
+      { url: "https://github.com/netooran", icon: "fab fa-github-square" },
+      { url: "https://www.linkedin.com/in/sarath-vs-935641ab", icon: "fab fa-linkedin" },
+      { url: "https://www.instagram.com/netooraan", icon: "fab fa-instagram" },
+      { url: "https://www.facebook.com/netooran", icon: "fab fa-facebook-square" },
+      { url: "https://twitter.com/SarathVS65", icon: "fab fa-twitter-square" }
+    ]
+  }
+});
+
 document.querySelectorAll("section").forEach(section =>
   section.querySelector("h1").addEventListener("click", event => {
     event.preventDefault();
